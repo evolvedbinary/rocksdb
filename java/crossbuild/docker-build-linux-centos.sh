53ee7d98506a77302b8d1e8186178eb0c35e1319
Adam Retter <adam.retter@googlemail.com>
2026-03-10 13:37:24 -0500

    [TEMPORARY] cherry-pick of 35148ac from branch: eb/feature/docker-build-for-GCC11+

--- java/crossbuild/docker-build-linux-centos.sh
+++ java/crossbuild/docker-build-linux-centos.sh
@@ -4,6 +4,11 @@
 set -e
 #set -x
 
+# Set job parallelism to 1 (none) if it is not defined in the environment
+if [ -z "${J}" ]; then
+  J=1
+fi
+
 # just in-case this is run outside Docker
 mkdir -p /rocksdb-local-build
 
@@ -11,27 +16,36 @@ rm -rf /rocksdb-local-build/{*,.[!.]*,..?*}
 cp -r /rocksdb-host/. /rocksdb-local-build
 pushd /rocksdb-local-build
 
-# Use scl devtoolset if available
+# Detect and enable devtoolset if available
+DEVTOOLSET=""
 if hash scl 2>/dev/null; then
-  if scl --list | grep -q 'devtoolset-8'; then
-                # CentOS 6+
-                scl enable devtoolset-8 'make clean-not-downloaded'
-                scl enable devtoolset-8 'PORTABLE=1 make -j2 rocksdbjavastatic'
+  # Check for devtoolsets in order of preference (newer first)
+  if scl --list | grep -q 'devtoolset-12'; then
+    DEVTOOLSET="devtoolset-12"
+  elif scl --list | grep -q 'devtoolset-11'; then
+    DEVTOOLSET="devtoolset-11"
+  elif scl --list | grep -q 'devtoolset-10'; then
+    DEVTOOLSET="devtoolset-10"
+  elif scl --list | grep -q 'devtoolset-9'; then
+    DEVTOOLSET="devtoolset-9"
+  elif scl --list | grep -q 'devtoolset-8'; then
+    DEVTOOLSET="devtoolset-8"
   elif scl --list | grep -q 'devtoolset-7'; then
-    # CentOS 6+
-    scl enable devtoolset-7 'make clean-not-downloaded'
-    scl enable devtoolset-7 'PORTABLE=1 make -j2 rocksdbjavastatic'
+    DEVTOOLSET="devtoolset-7"
   elif scl --list | grep -q 'devtoolset-2'; then
-    # CentOS 5 or 6
-    scl enable devtoolset-2 'make clean-not-downloaded'
-    scl enable devtoolset-2 'PORTABLE=1 make -j2 rocksdbjavastatic'
-  else
-    echo "Could not find devtoolset"
-    exit 1;
+    DEVTOOLSET="devtoolset-2"
   fi
+fi
+
+# Build with devtoolset if available, otherwise use system compiler
+if [ -n "$DEVTOOLSET" ]; then
+  echo "Using $DEVTOOLSET"
+  scl enable $DEVTOOLSET "bash -c 'make clean-not-downloaded'"
+  scl enable $DEVTOOLSET "bash -c 'PORTABLE=1 J=$J make -j$J rocksdbjavastatic'"
 else
+  echo "No SCL devtoolset found, falling back to system compiler"
   make clean-not-downloaded
-  PORTABLE=1 make -j2 rocksdbjavastatic
+  PORTABLE=1 J=$J make -j$J rocksdbjavastatic
 fi
 
 cp java/target-native/librocksdbjni-linux*.so /rocksdb-java-target
